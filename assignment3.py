a7f34f722cea06ab8d5b5fc7f27dca7b01f558b4
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-10-26 20:31:34 +0300

    Display Menu

--- assignment3.py
+++ assignment3.py
@@ -1,2 +1,13 @@
 name = input("Please enter your name: ")
 print("Welcome",name,"!")
+
+def displayMenu():
+  print("\n\t1. Add Matrices\n"
+        +"\t2. Check Rotation\n"
+        +"\t3. Invert Dictionary\n"
+        +"\t4. Convert Matrix to Dictionary\n"
+        +"\t5. Check Palindrome\n"
+        +"\t6. Search for an Element & Merge Sort\n"
+        +"\t7. Exit\n")
+
+displayMenu()
