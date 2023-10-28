514c7dbe7c0974a11754bee7a03b1db3f3ea6a63
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-10-28 21:04:00 +0300

    Merge sort

--- assignment3.py
+++ assignment3.py
@@ -80,6 +80,39 @@ def checkPalindrome():
   else:
     print(text,"is not a palindrome")
 
+######################################################################################
+
+def mergeSort(arr):
+  if len(arr) > 1:
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    mergeSort(left_half)
+    mergeSort(right_half)
+
+    i = j = k = 0
+
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            arr[k] = left_half[i]
+            i += 1
+        else:
+            arr[k] = right_half[j]
+            j += 1
+        k += 1
+
+    while i < len(left_half):
+        arr[k] = left_half[i]
+        i += 1
+        k += 1
+
+    while j < len(right_half):
+        arr[k] = right_half[j]
+        j += 1
+        k += 1
+
+######################################################################################
 
 name = input("Please enter your name: ")
 print("Welcome",name,"!")
@@ -108,5 +141,8 @@ def menu():
   elif choice == 5:
     checkPalindrome()
   elif choice == 6:
-    print("Search for an Element & Merge Sort")
+    user_input = input("Enter a list of numbers separated by commas: ")
+    arr = user_input.split(',')
+    mergeSort(arr)
+    print("Sorted list:", arr)
 menu()
