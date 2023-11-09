17716f32da737ab3ca10192807e862f2fbf423a3
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-09 20:00:13 +0200

    Display menu

--- midterm.py
+++ midterm.py
@@ -1,1 +1,15 @@
 print("Welcome!")
+print()
+
+def displayMenu():
+  print("\t1. Open Tab\n"
+        +"\t2. Close Tab\n"
+        +"\t3. Switch Tab\n"
+        +"\t4. Display All Tabs\n"
+        +"\t5. Open Nested Tab\n"
+        +"\t6. Clear All Tabs\n"
+        +"\t7. Save Tabs\n"
+        +"\t8. Import Tabs\n"
+        +"\t9. Exit\n")
+
+displayMenu()
