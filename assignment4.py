85c74901b24da75c464dc6f39d59052818150c2f
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-19 13:37:58 +0200

    Display the menu

--- assignment4.py
+++ assignment4.py
@@ -1,2 +1,12 @@
 name = input("Please enter your name: ")
 print("Welcome",name,"!")
+
+def displayMenu():
+  print("\n\t1. Singly Linked List\n"
+        +"\t2. Check if Palindrome\n"
+        +"\t3. Priority Queue\n"
+        +"\t4. Evaluate an Infix Expression\n"
+        +"\t5. Graph\n"
+        +"\t6. Exit\n")
+  
+displayMenu()
